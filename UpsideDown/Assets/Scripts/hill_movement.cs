ea478811466ec8bce17ddda966f28f1e45ffc862
Martin Håkansson <marhaka@kth.se>
2024-04-10 17:14:12 +0200

    added assets

---
+++ UpsideDown/Assets/Scripts/hill_movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hill_movement : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject camera;
+
+    public float speed;
+    public int height;
+
+    Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = new Vector3(0, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float player_x = player.transform.position.x;
+
+        position.x = player_x * speed;
+        position.y = player.transform.position.y + height;
+        transform.position = position;
+
+    }
+}
