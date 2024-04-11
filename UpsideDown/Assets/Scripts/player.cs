78ab263a9aaf064dd10e91415981d43ff35e37b6
Martin Håkansson <marhaka@kth.se>
2024-04-11 17:22:22 +0200

    textures light and some other things

--- UpsideDown/Assets/Scripts/player.cs
+++ UpsideDown/Assets/Scripts/player.cs
@@ -10,22 +10,59 @@ public class player : MonoBehaviour
     public float speed;
     public float speedJump;
     bool onGround;
-    bool goodWorld;
+    string current_world;
+
+    public GameObject Camera;
+    PlayerCamera camera_controller;
+
+    public GameObject bullet;
+
+    private int looking_direction;
+
+    public enum WorldType
+    {
+        GoodWorld,
+        BadWorld
+    }
+    public WorldType start_world;
+
 
     public SoundManager soundManager;
     // Start is called before the first frame update
     void Start()
     {
-        goodWorld = true;
+        looking_direction = 1;
         onGround = false;
         rb = GetComponent<Rigidbody2D>();
+        camera_controller = Camera.GetComponent<PlayerCamera>();
+        rb.freezeRotation = true;
+
+
+        if (start_world == WorldType.GoodWorld)
+        {
+            current_world = "good";
+        }
+        else
+        {
+            current_world = "bad";
+        }
+        camera_controller.update_current_world(current_world);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        rb.AddForce(movement, ForceMode2D.Force);
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+
+        if (horizontalMovement > 0)
+        {
+            looking_direction = 1; // Right
+        }
+        else if (horizontalMovement < 0)
+        {
+            looking_direction = -1; // Left
+        }
+        rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+
 
         if (Input.GetButtonDown("Jump") && onGround)
         {
@@ -33,25 +70,37 @@ public class player : MonoBehaviour
             soundManager.PlayJumpSound();
         }
 
-        if (Input.GetButtonDown("Fire1")) // change from good to bad
+        if (Input.GetButtonDown("Fire2")) // change from good to bad
         {
-            Debug.Log("FIRE");
-            Debug.Log(goodWorld);
-            switch (goodWorld)
-            {
-                case true:
-                    Debug.Log("FIRE 1");
-                    goodWorld = false;
-                    transform.position += new Vector3(0, -15, 0);
-                    break;
-                case false:
-                    Debug.Log("FIRE 1");
-                    goodWorld = true;
-                    transform.position += new Vector3(0, 15, 0);
-                    break;
-            }
+            change_world();
+        }
+        if (Input.GetButtonDown("Fire1") && current_world == "bad")
+        {
+            fire_bullet();
         }
 
+    }
+    void fire_bullet()
+    {
+        GameObject bullet_instance = Instantiate(bullet, transform.position, Quaternion.identity);
+        Bullet bullet_script = bullet_instance.GetComponent<Bullet>();
+        bullet_script.set_direction(looking_direction); // sets direction of bullet, 1 right -1 left
+    }
+
+    void change_world()
+    {
+        switch (current_world)
+        {
+            case "good":
+                current_world = "bad";
+                transform.position += new Vector3(0, -20, 0);
+                break;
+            case "bad":
+                current_world = "good";
+                transform.position += new Vector3(0, 20, 0);
+                break;
+        }
+        camera_controller.update_current_world(current_world);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
